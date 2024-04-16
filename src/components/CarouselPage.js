e7bf73f0d7c05987fba5338e09b728b652dc0714
MaxVives123 <maxvivesribera@gmail.com>
2024-04-16 15:15:37 +0200

    Carousel inicial

---
+++ src/components/CarouselPage.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import Carousel from 'react-bootstrap/Carousel';
+import img1 from '../images/img1.jpg'
+import './CarouselPage.css'; // Importando el archivo CSS local
+
+function CarouselPage() {
+  return (
+    
+    <Carousel fade>
+    <Carousel.Item>
+      <img style={{height:'90vh'}}
+        className="d-block w-100"
+        src={img1}
+        alt="First slide"
+      />
+      <Carousel.Caption>
+        <h3>First slide label</h3>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img style={{height:'90vh'}}
+        className="d-block w-100"
+        src={img1}
+        alt="Second slide"
+      />
+
+      <Carousel.Caption>
+        <h3>Second slide label</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img style={{height:'90vh'}}
+        className="d-block w-100"
+        src={img1}
+        alt="Third slide"
+      />
+
+      <Carousel.Caption>
+        <h3>Third slide label</h3>
+        <p>
+          Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+        </p>
+      </Carousel.Caption>
+    </Carousel.Item>
+  </Carousel>
+  
+  )
+}
+
+export default CarouselPage
