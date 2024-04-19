78e2eea0d71ece0b7955df0642b4c02bc94c627c
MaxVives123 <maxvivesribera@gmail.com>
2024-04-19 20:46:53 +0200

    Funciona Visualizer falta optimitzar

--- src/components/Model3d.js
+++ src/components/Model3d.js
@@ -1,20 +1,25 @@
 import React, { useEffect } from 'react';
-
+import { Viewer, Entity, PointGraphics, EntityDescription } from 'resium';
+import { Cartesian3 } from 'cesium';
 const Model3d = () => {
-  useEffect(() => {
-    const loadCss = async () => {
-      const cssUrl = 'https://cesium.com/downloads/cesiumjs/releases/1.113/Build/Cesium/Widgets/widgets.css';
-      await import(cssUrl);
-    };
 
-    loadCss();
-  }, []);
+return(
+  <div style={{ width: '100%', height: '10vh', position: 'relative' }}>
+  <Viewer cover>
+    <Entity
+      name="BoxGraphics"
+      position={Cartesian3.fromDegrees(0, 0, 0)}
+    >
+      <PointGraphics pixelSize={10} />
+      <EntityDescription>
+        <h1>Hello</h1>
+      </EntityDescription>
+    </Entity>
+  </Viewer>
+</div>
+)
 
-  return (
-    <div id="cesiumContainer" style={{ width: '100%', height: '500px' }}>
-      {/* Cesium Viewer will be initialized here */}
-    </div>
-  );
+
 };
 
 export default Model3d;
